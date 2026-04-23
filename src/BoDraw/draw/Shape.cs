e6be0473cbf4a83843eb4f6c5f40030e6f9aa7ec
Matthias Baitsch <matthias.baitsch@me.com>
2026-04-23 06:51:43 +0200

    Add docstring

--- src/BoDraw/draw/Shape.cs
+++ src/BoDraw/draw/Shape.cs
@@ -29,8 +29,18 @@ public abstract class Shape
 
     public abstract Rect Bounds { get; }
 
+    /// <summary>
+    /// Moves the shape by the given offset.
+    /// </summary>
+    /// <param name="dx">Horizontal offset.</param>
+    /// <param name="dy">Vertical offset.</param>
     public abstract void Move(double dx, double dy);
 
+    /// <summary>
+    /// Returns a copy of the shape moved by the given offset.
+    /// </summary>
+    /// <param name="dx">Horizontal offset.</param>
+    /// <param name="dy">Vertical offset.</param>
     public Shape Copy(double dx, double dy)
     {
         var copy = this.DeepClone();
