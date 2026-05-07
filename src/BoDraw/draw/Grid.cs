d779960d20a154ed677b0da9cfd868579ae22555
Matthias Baitsch <matthias.baitsch@me.com>
2026-05-07 22:54:51 +0200

    .

--- src/BoDraw/draw/Grid.cs
+++ src/BoDraw/draw/Grid.cs
@@ -12,15 +12,29 @@ namespace BoDraw;
 /// <code>
 /// using BoDraw;
 ///
-/// Grid grid = new Grid(new Circle(0.5, 0.5, 0.4));
-/// grid.Nx = 4;
-/// grid.Dx = 1.5;
-/// grid.Ny = 3;
-/// grid.Dy = 1.5;
+/// BoDrawApp app = new BoDrawApp();
+/// 
+/// Rectangle r1 = new Rectangle(0, 0, 1.5, 2.0 / 3.0);
+/// r1.FillColor = Colors.HotPink;
 ///
-/// BoDrawApp bd = new BoDrawApp();
-/// bd.Add(grid);
-/// bd.Show();
+/// Rectangle r2 = new Rectangle(0, 1.0 / 3.0, 1.5, 1);
+/// r2.FillColor = Colors.Orange;
+/// r2.FillOpacity = 0.7;
+///
+/// Circle c = new Circle(0.5, 0.5, 0.5);
+/// c.FillColor = Colors.FromRgb(20, 255, 140);
+/// c.FillOpacity = 0.3;
+///
+/// Group group = new Group(r1, r2, c);
+///
+/// Grid grid = new Grid(group);
+/// grid.Nx = 10;
+/// grid.Dx = 1.6;
+/// grid.Ny = 15;
+/// grid.Dy = 1.1;
+///
+/// app.Add(grid);
+/// app.Show();
 /// </code>
 /// </example>
 public class Grid : Shape
